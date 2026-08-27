0cfd6d2f65036c094b76756da17daf543283389d
gchang12 <gc8@hawaii.edu>
2026-08-26 18:14:52 -1000

    Renamed code for more semantic clarity

--- pony-grep/src/pages/TranscriptSearch.vue
+++ pony-grep/src/pages/TranscriptSearch.vue
@@ -37,7 +37,7 @@
       //console.log(el.innerHTML);
       //console.log("vHighlight: re", re);
       //console.log("vHighlight: el.innerHTML", el.innerHTML);
-      el.innerHTML = el.innerHTML.replaceAll(re, (match) => "<span class='highlighted'>" + match + "</span>" );
+      el.innerHTML = el.innerHTML.replaceAll(re, (match) => "<span class='PatternMatch'>" + match + "</span>" );
     }
   }
 
