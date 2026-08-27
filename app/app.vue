128dceb931d4afba9d7e236b81117bf3ea4495ce
gchang12 <gc8@hawaii.edu>
2026-08-27 11:10:49 -1000

    Rearranged layouts

--- app/app.vue
+++ app/app.vue
@@ -2,11 +2,9 @@
 
   /*
   import { ref, computed } from "vue";
-
   import IndexPage from './pages/IndexPage.vue'
   import EpisodeDirectory from './pages/EpisodeDirectory.vue'
   import TranscriptSearch from './pages/TranscriptSearch.vue'
-
   // Excerpted from code in video demonstration.
   // Start: Routing.
   const routes = {
@@ -41,58 +39,8 @@
 
 <template>
   <div id="app">
-    <!-- <NuxtRouteAnnouncer /> -->
-    <!-- <NuxtWelcome /> -->
-    <header>
-      <!-- Bootstrap: 'container' declares grid layout; 1 row, 2 columns. (Logo, Navibar) -->
-      <div class="container"> 
-        <div class="NaviBar row justify-content-between align-items-center">
-          <!-- Bootstrap: 'col-auto' makes container width shrink to fit contents -->
-          <div class="col-auto">
-            <a href="/" class="img-link Logo">
-              <img src="./assets/banner.png" />
-            </a>
-          </div>
-          <div class="col-auto">
-            <nav>
-              <!-- Bootstrap: 'nav' makes container an actual navibar -->
-              <ul class="nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="/search">Search</a>
-                </li>
-                <li class="nav-item">
-                  <!-- Bootstrap: 'nav-link' provides v-centering. -->
-                  <a class="nav-link" href="/episode-transcripts">Transcripts</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div> 
-    </header>
-    <main>
+    <NuxtLayout>
       <NuxtPage />
-    </main>
-    <footer>
-      <article class="Credits">
-        <span class="FooterSectionHeader">
-          Credits
-        </span>
-        <ul>
-          <li>
-            All textual content is owned by <a target="_blank" class="External" href="https://www.fandom.com/licensing">Fandom</a>
-          </li>
-          <li>
-            This project's <a target="_blank" class="External" href="https://github.com/gchang12/html300-2024/tree/lesson8">GitHub</a> page
-          </li>
-          <li>
-            Site logo provided by <a href="https://fontmeme.com/friendship-is-magic-font/" target="_blank" class="External">this site</a>
-          </li>
-        </ul>
-        <div class="Disclaimer">All images are owned by Hasbro</div>
-        <div class="Disclaimer">Made with no AI</div>
-      </article>
-    </footer>
+    </NuxtLayout>
   </div>
 </template>
-
