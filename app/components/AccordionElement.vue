26bea513a91658f59d0c973335b5e2b0eb78451d
gchang12 <gc8@hawaii.edu>
2026-08-27 11:10:20 -1000

    Fixed accordion

--- app/components/AccordionElement.vue
+++ app/components/AccordionElement.vue
@@ -12,24 +12,24 @@
 </script>
 
 <template>
-  <div class="accordion">
-    <div class="accordion-item">
-      <h3 class="accordion-header">
+  <Accordion parent>
+    <AccordionItem>
+      <AccordionHeader level="3">
         <!-- Bootstrap: 'd-flex', 'justify-content-between' to separate series title and result-count.-->
         <!-- Bootstrap: 'btn-block' so that it spans entire width of container.-->
-        <b-button v-b-toggle="'collapse-' + collapseKey" :data-series="seriesName" class="ShowResults d-flex justify-content-between btn btn-lg btn-block accordion-button" aria-expanded="true" :aria-controls="'collapse-' + collapseKey">
+        <AccordionButton :data-series="seriesName" class="ShowResults d-flex justify-content-between btn btn-lg btn-block accordion-button" aria-expanded="true" :aria-controls="'collapse-' + collapseKey">
           <slot name="header">
           </slot>
-        </b-button>
-      </h3>
-      <b-collapse :id="'collapse-' + collapseKey" :aria-labelledby="'heading-' + collapseKey">
-        <b-card class="SearchResults accordion-body">
+        </AccordionButton>
+      </AccordionHeader>
+      <AccordionCollapse :id="'collapse-' + collapseKey" :aria-labelledby="'heading-' + collapseKey">
+        <AccordionBody class="SearchResults accordion-body">
           <!-- Bootstrap: 'table-striped' for line visibility. -->
           <slot name="body">
           </slot>
-        </b-card>
-      </b-collapse>
-    </div>
-  </div>
+        </AccordionBody>
+      </AccordionCollapse>
+    </AccordionItem>
+  </Accordion>
 </template>
 
