95ab98729c9cd044ad622022d713dc0fa502a5ad
gchang12 <gc8@hawaii.edu>
2026-08-29 07:25:12 -1000

    Wrote rudimentary layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,64 +1,27 @@
 <script setup>
   import Banner from '~/assets/images/banner.png';
-
 </script>
 
 <template>
   <div>
-    <!-- <NuxtRouteAnnouncer /> -->
-    <!-- <NuxtWelcome /> -->
    <header>
-      <!-- Bootstrap: 'container' declares grid layout; 1 row, 2 columns. (Logo, Navibar) -->
-      <div class="container"> 
-        <div class="NaviBar row justify-content-between align-items-center">
-          <!-- Bootstrap: 'col-auto' makes container width shrink to fit contents -->
-          <div class="col-auto">
-            <NuxtLink to="/" class="img-link Logo">
-            <!-- <img src="./assets/banner.png" /> -->
-              <img :src="Banner" />
-            </NuxtLink>
-          </div>
-          <div class="col-auto">
-            <nav>
-              <!-- Bootstrap: 'nav' makes container an actual navibar -->
-              <ul class="nav">
-                <li class="nav-item">
-                  <NuxtLink class="nav-link" to="/search">Search</NuxtLink>
-                </li>
-                <li class="nav-item">
-                  <!-- Bootstrap: 'nav-link' provides v-centering. -->
-                  <NuxtLink class="nav-link" to="/episode-index">Episode Index</NuxtLink>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div> 
-    </header>
-    <main>
-      <slot />
-    </main>
-    <footer>
-      <article class="Credits">
-        <span class="FooterSectionHeader">
-          Credits
-        </span>
-        <ul>
-          <li>
-            All textual content is owned by <a target="_blank" class="External" href="https://www.fandom.com/licensing">Fandom</a>
-          </li>
-          <li>
-            This project's <a target="_blank" class="External" href="https://github.com/gchang12/html300-2024/tree/lesson8">GitHub</a> page
-          </li>
-          <li>
-            Site logo provided by <a href="https://fontmeme.com/friendship-is-magic-font/" target="_blank" class="External">this site</a>
-          </li>
-        </ul>
-        <div class="Disclaimer">All images are owned by Hasbro</div>
-        <div class="Disclaimer">Made with no AI</div>
-      </article>
-    </footer>
+     <NuxtLink to="/">
+       <img :src="Banner" />
+     </NuxtLink>
+     <nav>
+       <NuxtLink to="/search">Search</NuxtLink>
+       <NuxtLink to="/episode-index">Episode Index</NuxtLink>
+     </nav>
+   </header>
+   <main>
+     <slot />
+   </main>
+   <footer>
+     All textual content is owned by <a target="_blank" href="https://www.fandom.com/licensing">Fandom</a>
+     This project's <a target="_blank" href="https://github.com/gchang12/pony-grep">GitHub</a> page
+     Site logo provided by <a target="_blank" href="https://fontmeme.com/friendship-is-magic-font/">this site</a>
+     All images are owned by Hasbro
+     Made with no AI
+   </footer>
   </div>
 </template>
-
-
