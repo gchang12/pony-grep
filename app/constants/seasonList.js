61dfafa0980c8068b80f544f4c696624eeb46936
gchang12 <gc8@hawaii.edu>
2026-08-28 14:52:25 -1000

    Saved season list

---
+++ app/constants/seasonList.js
@@ -0,0 +1,32 @@
+
+/*
+EqG
+===
+My Little Pony Equestria Girls
+My Little Pony Equestria Girls: Rainbow Rocks
+My Little Pony Equestria Girls: Friendship Games
+My Little Pony Equestria Girls: Legend of Everfree
+Specials
+Better Together (season 1)
+Better Together (season 2)
+Choose Your Own Ending (season 1)
+Choose Your Own Ending (season 2)
+Summertime Shorts
+
+FiM
+===
+S1
+S2
+S3
+S4
+S5
+S6
+S7
+S8
+S9
+Shorts
+Specials
+The Movie
+FiF
+Best Gift Ever
+*/
