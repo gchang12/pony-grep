018979e5034219e5477999282f50dcb2b7a5a351
gchang12 <gc8@hawaii.edu>
2026-08-29 14:00:15 -1000

    Added animation type list"

---
+++ app/functions/getAnimationTypes.js
@@ -0,0 +1,24 @@
+export default function() {
+  return [
+    {
+      "name": "clipshow",
+      "alias": "CS",
+    },
+    {
+      "name": "film",
+      "alias": "MV",
+    },
+    {
+      "name": "episode",
+      "alias": "EP",
+    },
+    {
+      "name": "special",
+      "alias": "SP",
+    },
+    {
+      "name": "short",
+      "alias": "SH",
+    },
+  ];
+}
