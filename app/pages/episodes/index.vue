1c0e05c5c0c1bda4a2a41407f25febd1e58752e4
gchang12 <gc8@hawaii.edu>
2026-08-30 15:08:24 -1000

    Inserted season images into episode index

--- app/pages/episodes/index.vue
+++ app/pages/episodes/index.vue
@@ -1,7 +1,30 @@
 <script setup>
+
   import seasonList from "../../assets/json/seasonList.json";
+
+  const response2 = await useFetch("json/animationIndex.json");
+
 </script>
 
 <template>
-  <h1>Episode Index</h1>
+  <div id="episodes">
+    <h1>Episodes</h1>
+    <article>
+      <h2>Friendship is Magic</h2>
+      <ol>
+        <!-- Each of these has to be a dropdown menu. -->
+        <li class="ImageDropdown" :id="season.urlName" v-for="season in seasonList.filter(season => season.series === 'FiM')" :key="season.urlName">
+          {{ season.name }}
+        </li>
+      </ol>
+    </article>
+    <article>
+      <h2>Equestria Girls</h2>
+      <ol>
+        <li class="ImageDropdown" :id="season.urlName" v-for="season in seasonList.filter(season => season.series === 'EqG')" :key="season.urlName">
+          {{ season.name }}
+        </li>
+      </ol>
+    </article>
+  </div>
 </template>
