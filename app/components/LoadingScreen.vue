38f3ba0f670925521c96f60fc9b679ccf811870f
gchang12 <gc8@hawaii.edu>
2026-08-31 18:35:29 -1000

    Implemented rudimentary loading screen. Not sure if I'll get to use it.

--- app/components/LoadingScreen.vue
+++ app/components/LoadingScreen.vue
@@ -1,5 +1,12 @@
 <script setup>
+  defineProps({
+    message: String,
+  });
 </script>
 
 <template>
+  <div class="LoadingScreen">
+    <NuxtLoadingIndicator />
+    <span class="LoadingMessage">{{ message }}</span>
+  </div>
 </template>
