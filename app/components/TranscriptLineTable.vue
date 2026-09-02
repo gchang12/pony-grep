d91f0ec63e2e8ca118fa5093618ff57807bd099b
gchang12 <gc8@hawaii.edu>
2026-09-01 22:56:20 -1000

    Inserted new field into transcript table

--- app/components/TranscriptLineTable.vue
+++ app/components/TranscriptLineTable.vue
@@ -10,12 +10,16 @@
   <table>
     <thead>
       <tr>
+        <th>Link</th>
         <th>Character</th>
         <th>Line</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="tline in transcriptLines.filter(tline => tline.episodeId === episode.id)" :key="tline.id" :id="'L' + tline.lineNo" :class="jumpedLine == tline.lineNo ? 'JumpedLine' : ''">
+        <td>
+          <NuxtLink :href="'#L' + tline.lineNo">#</NuxtLink>
+        </td>
         <th>
           {{ tline.speaker }}
         </th>
