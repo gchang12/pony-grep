c4e3a74739cf06e742fbcbe68f827a079d9a077d
gchang12 <gc8@hawaii.edu>
2026-09-02 21:26:03 -1000

    Clumsily implemented new active-row-highlighting mechanism.

--- app/components/TranscriptLineTable.vue
+++ app/components/TranscriptLineTable.vue
@@ -6,21 +6,21 @@
 </script>
 
 <template>
-  <table class="TranscriptLineTable table table-striped">
+  <table class="TranscriptLineTable table table-striped table-dark">
     <tbody>
-      <tr v-for="tline in transcriptLines" :key="tline.id" :id="'L' + tline.lineNo">
+      <tr v-for="tline in transcriptLines" :key="tline.id" :id="'L' + tline.lineNo" :class="jumpedLine == tline.lineNo ? 'table-info' : ''">
         <td>
           <NuxtLink :href="'#L' + tline.lineNo">#</NuxtLink>
         </td>
         <th>
-          <span :class="jumpedLine == tline.lineNo ? 'JumpedLine' : ''">
+          <!-- <span :class="jumpedLine == tline.lineNo ? 'JumpedLine' : ''"> -->
             {{ tline.speaker }}
-          </span>
+            <!-- </span> -->
         </th>
         <td>
-          <span :class="jumpedLine == tline.lineNo ? 'JumpedLine' : ''">
+          <!-- <span :class="jumpedLine == tline.lineNo ? 'JumpedLine' : ''"> -->
             {{ tline.dialogue }}
-          </span>
+            <!-- </span> -->
         </td>
       </tr>
     </tbody>
