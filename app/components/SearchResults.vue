2f1d4d3587160fd4dec0a1826dcd941404c5b67c
gchang12 <gc8@hawaii.edu>
2026-09-04 10:45:54 -1000

    Added pagination to search-results

--- app/components/SearchResults.vue
+++ app/components/SearchResults.vue
@@ -1,11 +1,25 @@
 <script setup>
 
+  import { ref } from "vue";
+
   const props = defineProps({
     items: Array,
     vHighlight: Object,
   });
 
   const vHighlight = props.vHighlight;
+
+  const maxResultCount = 100;
+
+  const pageRange = ref([0, maxResultCount]);
+
+  function incrementPageRange() {
+    pageRange.value = [pageRange.value[0] + maxResultCount, pageRange.value[1] + maxResultCount];
+  }
+
+  function decrementPageRange() {
+    pageRange.value = [pageRange.value[0] - maxResultCount, pageRange.value[1] - maxResultCount];
+  }
 
 </script>
 
@@ -21,7 +35,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="tline in item.content" :key="tline.id">
+          <tr v-for="tline in item.content.slice(...pageRange)" :key="tline.id">
             <th>{{tline.speaker}}</th>
             <td v-highlight>{{tline.dialogue}}</td>
             <td>
@@ -32,6 +46,19 @@
           </tr>
         </tbody>
       </table>
+      <div class="d-flex justify-content-between" v-if="item.content.length > 100">
+        <button class="btn w-50 btn-secondary" type="button" v-if="pageRange[0] > 0" @click="decrementPageRange">
+          Previous
+        </button>
+        <div class="invisible" v-else></div>
+        <button class="btn w-50 btn-secondary" type="button" v-if="pageRange[1] < item.content.length" @click="incrementPageRange">
+          Next
+        </button>
+        <div class="invisible" v-else></div>
+      </div>
+      <div class="PageRange w-100 text-center">
+        Results {{ pageRange[0] + 1}} – {{ pageRange[1] }}
+      </div>
     </template>
   </UAccordion>
 </template>
