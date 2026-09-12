727027965919c846a6d5e60d6cdf7672e488bc58
gchang12 <gc8@hawaii.edu>
2026-09-12 08:26:11 -1000

    Added Google Analytics tag

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,6 +4,14 @@
 
 <template>
   <div id="app">
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-9GH7X9N0XK"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-9GH7X9N0XK');
+  </script>
    <header>
      <div class="container-sm">
        <div class="row flex-sm-row flex-column">
